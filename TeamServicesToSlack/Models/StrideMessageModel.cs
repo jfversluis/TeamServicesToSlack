09d6704c95ecba6ff172640e542cf274d19ae225
Gerald Versluis <gerald@verslu.is>
2018-04-16 19:36:23 +0200

    Implement Stride message as a model

--- TeamServicesToSlack/Models/StrideMessageModel.cs
+++ TeamServicesToSlack/Models/StrideMessageModel.cs
@@ -1,37 +1,115 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 
 namespace TeamServicesToSlack.Models
 {
-    public class StrideMessageModel
-    {
-        public string username { get; set; }
-        public string icon_emoji { get; set; }
-        public List<SlackAttachment> attachments { get; set; }
-        public string text { get; set; }
-        public string channel { get; set; }
+	public class StrideMessageModel
+	{
+		[JsonProperty("version")]
+		public long Version { get; set; }
 
-        public class SlackAttachment
-        {
-            public string color { get; set; }
-            public string title { get; set; }
-            public string text { get; set; }
-            public string pretext { get; set; }
-            public List<SlackField> fields { get; set; }
-            public long ts { get; set; }
-            public string[] mrkdwn_in { get; set; }
-            public SlackAction[] actions { get; set; }
-        }
+		[JsonProperty("type")]
+		public string Type { get; set; }
 
-        public class SlackAction
-        {
-            public string type { get; set; }
-            public string text { get; set; }
-            public string url { get; set; }
-        }
-        public class SlackField
-        {
-            public string title { get; set; }
-            public string value { get; set; }
-        }
-    }
+		[JsonProperty("content")]
+		public Content[] Content { get; set; }
+	}
+
+	public class Content
+	{
+		[JsonProperty("type")]
+		public string Type { get; set; }
+
+		[JsonProperty("attrs")]
+		public Attrs Attrs { get; set; }
+	}
+
+	public class Attrs
+	{
+		[JsonProperty("actions")]
+		public Action[] Actions { get; set; }
+
+		[JsonProperty("text")]
+		public string Text { get; set; }
+
+		[JsonProperty("link")]
+		public Link Link { get; set; }
+
+		[JsonProperty("collapsible")]
+		public bool Collapsible { get; set; }
+
+		[JsonProperty("title")]
+		public Title Title { get; set; }
+
+		[JsonProperty("description")]
+		public Description Description { get; set; }
+
+		[JsonProperty("context")]
+		public Context Context { get; set; }
+
+		[JsonProperty("preview")]
+		public Link Preview { get; set; }
+	}
+
+	public class Action
+	{
+		[JsonProperty("key")]
+		public string Key { get; set; }
+
+		[JsonProperty("title")]
+		public string Title { get; set; }
+
+		[JsonProperty("target")]
+		public Target Target { get; set; }
+	}
+
+	public class Target
+	{
+		[JsonProperty("key")]
+		public string Key { get; set; }
+	}
+
+	public class Context
+	{
+		[JsonProperty("text")]
+		public string Text { get; set; }
+
+		[JsonProperty("icon")]
+		public Icon Icon { get; set; }
+	}
+
+	public class Icon
+	{
+		[JsonProperty("url")]
+		public string Url { get; set; }
+
+		[JsonProperty("label")]
+		public string Label { get; set; }
+	}
+
+	public class Description
+	{
+		[JsonProperty("text")]
+		public string Text { get; set; }
+	}
+
+	public class Link
+	{
+		[JsonProperty("url")]
+		public string Url { get; set; }
+	}
+
+	public class Title
+	{
+		[JsonProperty("text")]
+		public string Text { get; set; }
+
+		[JsonProperty("user")]
+		public User User { get; set; }
+	}
+
+	public class User
+	{
+		[JsonProperty("icon")]
+		public Icon Icon { get; set; }
+	}
 }
--- TeamServicesToSlack/TeamServicesToStride.cs
+++ TeamServicesToSlack/TeamServicesToStride.cs
@@ -19,124 +19,137 @@ using TeamServicesToSlack.Services;
 namespace TeamServicesToSlack
 {
 	public static partial class TeamServicesToStride
-    {
-        [FunctionName("TeamServicesToStride")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
-        {
-            log.Info("C# HTTP trigger function processed a request.");
+	{
+		[FunctionName("TeamServicesToStride")]
+		public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+		{
+			log.Info("C# HTTP trigger function processed a request.");
 			bool simple = !(req.Headers.FirstOrDefault(h => h.Key.ToLowerInvariant() == "format").Value?.ToString().ToLowerInvariant() == "extended");
 
 			string requestBody = new StreamReader(await req.Content.ReadAsStreamAsync()).ReadToEnd();
-            var request = JsonConvert.DeserializeObject<WebhookRequest>(requestBody);
-            //var message = data?.message?.text;
-            var detailedMessage = request.DetailedMessage.text;
-            // important - get this data
-            var resourceLink = request.Resource.url;
-            //var changesData = await GetVstsResource($"{resourceLink}/changes");
-            //var changes = JsonConvert.DeserializeObject<Changes>(changesData);
-            //var changesCount = changes.count;
-            //var changesText = "";
-            //foreach (var change in changes.value)
-            //{
-            //    changesText +=
-            //        $"<{change.displayUri}|{change.message}> by *{change.Author.displayName}*\n";
-            //    if (!simple)
-            //        changesText += $"{change.message}\n\n";
-            //}
-            //var resource = await GetVstsResource(resourceLink);
-            //var resourceData = JsonConvert.DeserializeObject<ResourceDetails>(resource);
-            //var projectName = resourceData.Project.Name;
-            //var buildLink = resourceData._Links.Web.Href;
-            //var sourceBranch = resourceData.SourceBranch;
-            //var sourceVersion = resourceData.SourceVersion;
-            //var repository = resourceData.Repository.Id;
-            //var definitionName = request.Resource.definition.name;
-            //var gitUrl = $"{repository.Replace(".git", "")}/commit/{sourceVersion}";
+			var request = JsonConvert.DeserializeObject<WebhookRequest>(requestBody);
+			//var message = data?.message?.text;
+			var detailedMessage = request.DetailedMessage.text;
+			// important - get this data
+			var resourceLink = request.Resource.url;
+			//var changesData = await GetVstsResource($"{resourceLink}/changes");
+			//var changes = JsonConvert.DeserializeObject<Changes>(changesData);
+			//var changesCount = changes.count;
+			//var changesText = "";
+			//foreach (var change in changes.value)
+			//{
+			//    changesText +=
+			//        $"<{change.displayUri}|{change.message}> by *{change.Author.displayName}*\n";
+			//    if (!simple)
+			//        changesText += $"{change.message}\n\n";
+			//}
+			//var resource = await GetVstsResource(resourceLink);
+			//var resourceData = JsonConvert.DeserializeObject<ResourceDetails>(resource);
+			//var projectName = resourceData.Project.Name;
+			//var buildLink = resourceData._Links.Web.Href;
+			//var sourceBranch = resourceData.SourceBranch;
+			//var sourceVersion = resourceData.SourceVersion;
+			//var repository = resourceData.Repository.Id;
+			//var definitionName = request.Resource.definition.name;
+			//var gitUrl = $"{repository.Replace(".git", "")}/commit/{sourceVersion}";
 
-            //var timeline = await GetVstsResource(resourceData._Links.Timeline.Href);
-            //var timeLineData = JsonConvert.DeserializeObject<Timeline>(timeline);
-            //var failingTask = timeLineData.Records.FirstOrDefault(x => x.Result == "failed");
+			//var timeline = await GetVstsResource(resourceData._Links.Timeline.Href);
+			//var timeLineData = JsonConvert.DeserializeObject<Timeline>(timeline);
+			//var failingTask = timeLineData.Records.FirstOrDefault(x => x.Result == "failed");
 
 			var strideService = new StrideService(log, KeyManager.GetSecret("StrideWebhookUrl"));
-            var model = new StrideMessageModel
-            {
-                username = "The VSTS detective",
-                icon_emoji = ":vsts:",
-                //text = $"*{projectName}/{definitionName} - {failingTask.Name} failed*",
-                //channel = channel,
-                //attachments = new List<StrideMessageModel.SlackAttachment>()
-                //{
-                //    new StrideMessageModel.SlackAttachment
-                //    {
-                //        color = "#ff0000",
-                //        pretext = $"Repository: {repository}\nBranch: {sourceBranch}\nCommit: {sourceVersion}",
-                //        title = $"{changesCount} Change(s) in the build: ",
-                //        text = changesText,
-                //        actions = new[]
-                //        {
-                //            new StrideMessageModel.SlackAction
-                //            {
-                //                type = "button",
-                //                text = ":octocat: Git Repo Url",
-                //                url = repository
-                //            },
-                //            new StrideMessageModel.SlackAction
-                //            {
-                //                type = "button",
-                //                text = ":octocat: Git Commit Url",
-                //                url = gitUrl
-                //            },
-                //            new StrideMessageModel.SlackAction
-                //            {
-                //                type = "button",
-                //                text = ":vsts: VSTS Url",
-                //                url = buildLink
-                //            }
-                //        }
-                //    }
-                //}
-            };
-            if (!simple)
-            {
-                model.attachments.Add(new StrideMessageModel.SlackAttachment
-                {
-                    color = "#ff0000",
-                    title = "Build message",
-                    text = $"{detailedMessage}"
-                });
-            }
+			var model = new StrideMessageModel
+			{
+				Version = 1,
+				Type = "doc",
+				Content = new[] {
+						new Content
+						{
+							Type = "applicationCard",
+							Attrs = new Attrs
+							{
+								Actions = new[]
+								{
+									new Models.Action
+									{
+										Key = "unique-app-card-action-key",
+										Target = new Target
+										{
+											Key = "app-dialog"
+										},
+										Title = "Show VSTS"
+									}
+								},
+								Text = "Preview text",
+								Link = new Models.Link
+								{
+									Url = "https://dotcontrol.visualstudio.com"
+								},
+								Collapsible = true,
+								Title = new Title
+								{
+									Text = "Someone broke the build!",
+									User = new User
+									{
+										Icon = new Icon
+										{
+											Url = "https://www.gravatar.com/avatar/c3c9338e575a73892b0f1257eb9ee997",
+											Label = "Someone"
+										}
+									}
+								},
+								Description = new Description
+								{
+									Text = "Bla"
+								},
+								Preview = new Models.Link
+								{
+									Url = "https://vignette.wikia.nocookie.net/cardfight/images/2/2c/Sad_panda.jpg/revision/latest?cb=20140720193511"
+								},
+							Context = new Context
+							{
+								Icon = new Icon
+								{
+									Url = "https://www.delta-n.nl/actueel/blogs/development-blog/PublishingImages/VSTSlogobijTips.png",
+									Label = "VSTS"
+								},
+								Text = "Build failed"
+							}
+							}
+						}
+				}
+			};
 
+			strideService.PostToStride(model);
+			return new OkObjectResult("");
+		}
 
-            strideService.PostToStride(model);
-            return new OkObjectResult("");
-        }
+		public static async Task<string> GetVstsResource(string url)
+		{
+			try
+			{
+				var personalaccesstoken = KeyManager.GetSecret("VSTSPersonalAccessToken");
 
-        public static async Task<string> GetVstsResource(string url)
-        {
-            try
-            {
-                var personalaccesstoken = KeyManager.GetSecret("VSTSPersonalAccessToken");
+				using (HttpClient client = new HttpClient())
+				{
+					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                using (HttpClient client = new HttpClient())
-                {
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+					client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+						Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalaccesstoken))));
 
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-                        Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalaccesstoken))));
-
-                    using (HttpResponseMessage response = client.GetAsync(
-                        url).Result)
-                    {
-                        response.EnsureSuccessStatusCode();
-                        string responseBody = await response.Content.ReadAsStringAsync();
-                        return responseBody;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                return ex.ToString();
-            }
-        }
-    }
+					using (HttpResponseMessage response = client.GetAsync(
+						url).Result)
+					{
+						response.EnsureSuccessStatusCode();
+						string responseBody = await response.Content.ReadAsStringAsync();
+						return responseBody;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return ex.ToString();
+			}
+		}
+	}
 }
